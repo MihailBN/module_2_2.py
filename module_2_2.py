fa78145bcc7ecaf4bc0ae56a747e87bb906d8971
MihailBN <aratparosan@gmail.com>
2024-06-22 14:15:22 +0500

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,23 @@
+first = 123
+second = 456
+third = 789
+if first == second:
+    print('2')
+elif first == third:
+    print('2')
+elif second == third:
+    print('2')
+else:
+    print('0')
+    print('следующий этап')
+first = 42
+second = 69
+third = 42
+if first == second:
+    print('2')
+elif first == third:
+    print('2')
+elif second == third:
+    print('2')
+else:
+    print('0')
